e2304c11e470231f4210c382ccc759eb8749c016
Erik Nilsen <enilsen16@live.com>
2014-12-05 15:16:34 -0800

    final solution

--- whodunit.c
+++ whodunit.c
@@ -87,6 +87,13 @@ int main(int argc, char* argv[])
               triple.rgbtGreen = 0xff;
               triple.rgbtBlue = 0xff;
             }
+            if (triple.rgbtRed < 0xff)
+              {
+                triple.rgbtRed = 0x00;
+                triple.rgbtGreen = 0x00;
+                triple.rgbtBlue = 0x00;
+              }
+
 
             // write RGB triple to outfile
             fwrite(&triple, sizeof(RGBTRIPLE), 1, outptr);
