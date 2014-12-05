4036463147fc5ef922e5485bb7e2bfbf8b11ad25
Erik Nilsen <enilsen16@live.com>
2014-12-04 19:23:06 -0800

    initial solution

--- whodunit.c
+++ whodunit.c
@@ -81,6 +81,13 @@ int main(int argc, char* argv[])
             // read RGB triple from infile
             fread(&triple, sizeof(RGBTRIPLE), 1, inptr);
 
+            if (triple.rgbtRed == 0xff && triple.rgbtGreen == 0x00 && triple.rgbtBlue == 0x00)
+            {
+              triple.rgbtRed = 0xff;
+              triple.rgbtGreen = 0xff;
+              triple.rgbtBlue = 0xff;
+            }
+
             // write RGB triple to outfile
             fwrite(&triple, sizeof(RGBTRIPLE), 1, outptr);
         }
