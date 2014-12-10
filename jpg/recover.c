6857c6ed03b96347fdb92d681a792369e7f5f4fe
Erik Nilsen <enilsen16@live.com>
2014-12-09 20:03:57 -0800

    add pseudocode and open card.raw

--- jpg/recover.c
+++ jpg/recover.c
@@ -1,13 +1,40 @@
-/**
- * recover.c
- *
- * Computer Science 50
- * Problem Set 5
- *
- * Recovers JPEGs from a forensic image.
- */
+#include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char* argv[])
 {
-    // TODO
+  // ensure proper usage
+  if (argc != 1)
+  {
+    printf("Please remember that recover.c is for card.raw specifically\n");
+    return 1;
+  }
+
+  // open card.raw
+  FILE* card = fopen("card.raw", "r");
+  if ("card.raw" == NULL)
+  {
+    printf("Please make sure that card.raw exists");
+    return 2;
+  }
+
+  // declare the empty 512-integer array
+  unsigned int buffer[512];
+
+  // scanf("%d", buffer);
+  // Recrusively add 512 bytes into buffer
+  fread(&buffer, 512, 1, card);
+
+  // start of a new JPG
+  // Yes
+
+    // Open a new JPG
+    // Write the JPG
+    // Close and Loop agian
+
+  // No
+
+    //Go through the loop agian
+
+  //Close any remaining files
 }
